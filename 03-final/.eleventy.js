4fd7c266d83f43ebf6e28efded20d5c9ba905233
Robin Cussol <me@robincussol.com>
2022-10-25 20:55:57 +0200

    feat(03-final): use Eleventy Universal Filter instead
    This was suggested in a comment on my blog post!
    
    Using a data file to make `@11ty/eleventy-img` seemed hacky, so this is much
    more pleasing.
    
    I'm still hopeful that it would be possible to require `@11ty/eleventy-img`
    directly in the `_includes/webc/img.webc` file instead, to keep all image
    related concerns there.

--- 03-final/.eleventy.js
+++ 03-final/.eleventy.js
@@ -1,7 +1,26 @@
 const webc = require("@11ty/eleventy-plugin-webc");
+const Image = require("@11ty/eleventy-img");
 
-module.exports = function(eleventyConfig) {
-	eleventyConfig.addPlugin(webc, {
-		components: "_includes/webc/*.webc"
-	});
-}
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(webc, {
+    components: "_includes/webc/*.webc",
+  });
+  eleventyConfig.addFilter("Image", async (src, alt) => {
+    let metadata = await Image(src, {
+      widths: [300],
+      formats: ["avif", "jpeg"],
+      outputDir: "_site/img/",
+    });
+
+    let imageAttributes = {
+      alt: alt,
+      sizes: "100vw",
+      loading: "lazy",
+      decoding: "async",
+      "webc:raw": true,
+    };
+
+    // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+    return Image.generateHTML(metadata, imageAttributes);
+  });
+};
